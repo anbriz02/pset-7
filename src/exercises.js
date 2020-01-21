9b8e66041c25d8ad90fcb47a219b756645d7542f
andy on mac <55641032+anbriz02@users.noreply.github.com>
2020-01-20 22:57:55 -0500

    done maybe right ok sure ok yea no i hate this goodbye also this business plan is looking kind of slow also the research paper but that seems like a tomorrow problem ah hah hah

--- src/exercises.js
+++ src/exercises.js
@@ -1,22 +1,25 @@
 function commonEnd(a, b) {
   if (!a || a.length === 0 || !b || b.length === 0) {
     return false;
-  } else {
-    let aEnd = a.length - 1;
-    let bEnd = b.length - 1;
-    if (a[0] === b[0] || a[aEnd] === b[bEnd]) {
-      return true;
-    } else {
-      return false;
-    }
   }
-  // write your code here
+
+ let firstElement1= a[0]
+ let lastElement1= a[a.length - 1]
+
+ let firstElement2= b[0]
+ let lastElement2= b[b.length - 1]
+
+ if (firstElement1===firstElement2 || lastElement1===lastElement2 || a !== undefined && b !== undefined && a.length < 1 && b.length < 1){
+   return true;
+ } else {
+   return false;
+ }
 }
 
 function endsMeet(values, n) {
-  let empty = [];
+  let  nada = [];
   if (!values || values.length < n || n <= 0) {
-    return empty;
+    return  nada;
   } else if (n > 0) {
     let newArray = [];
     let first;
@@ -35,7 +38,7 @@ function endsMeet(values, n) {
     return values;
   }
 
-  // write your code here
+   
 }
 
 function difference(numbers) {
@@ -48,18 +51,18 @@ function difference(numbers) {
         return undefined;
       }
     }
-    let largest = Math.max.apply(null, numbers);
-    let smallest = Math.min.apply(null, numbers);
-    let difference = largest - smallest;
+    let biggest = Math.max.apply(null, numbers);
+    let chiquito = Math.min.apply(null, numbers);
+    let difference = biggest - chiquito;
     return difference;
   } else {
     return undefined;
   }
-  // write your code here
+   
 }
 
 function max(number) {
-  let numeral = true;
+  let numberthingy = true;
   if (!number || number.length < 3) {
     return undefined;
   }
@@ -71,11 +74,11 @@ function max(number) {
     for (i = 0; i <= number.length - 1; i++) {
       let unknown = number[i];
       if (isNaN(unknown)) {
-        numeral = false;
+        numberthingy = false;
       }
     }
   }
-  if (numeral === false) {
+  if (numberthingy === false) {
     return undefined;
   } else {
     let end = number.length - 1;
@@ -86,60 +89,57 @@ function max(number) {
     newArray.push(first);
     newArray.push(middle);
     newArray.push(last);
-    let largest = Math.max.apply(null, newArray);
-    return largest;
+    let biggest = Math.max.apply(null, newArray);
+    return biggest;
     }
-  // write your code here
+   
 }
 
 function middle(values) {
-  let empty = [];
-  if (!values || values.length < 3) {
-    return empty;
+  let array = [];
+
+  if (values == undefined) {
+
+    return [];
+
+  } else if (values.length < 3) {
+
+    return [];
+
+  } else if (values.length % 2 == 0) {
+    return [];
+  } else {
+    let middleee = values[values.length - (Math.ceil(values.length / 2))]
+    let middleonleft = values[(values.length - 1) - (Math.ceil(values.length / 2))]
+
+    array.push(middleonleft, middleee)
+
+    return array;
   }
-  let check = values.length % 2;
-  if (check === 0) {
-    return empty;
-  }
-  let end = values.length - 1;
-  let start = end / 2;
-  let middle = values[start];
-  let beforeMiddle = values[start - 1];
-  let afterMiddle = values[start + 1];
-  let newArray = [];
-  newArray.push(beforeMiddle);
-  newArray.push(middle);
-  newArray.push(afterMiddle);
-  return newArray;
-  // write your code here
 }
 
 function increasing(numbers) {
-  if (!numbers || numbers.length < 3) {
-    return false;
-  }
-  let numeral = true;
-  for (i = 0; i <= numbers.length - 1; i++) {
-    let unknown = numbers[i];
-    if (isNaN(unknown)) {
-      numeral = false;
-    }
-  }
-  if (numeral === false) {
-    return false;
-  }
-  let status = false;
-  for (x = 0; x < numbers.length - 1; x++) {
-    if (numbers[x] < numbers[x + 1] && numbers[x + 1] < numbers[x + 2]) {
-      status = true;
-    }
-  }
-  if (status === true) {
-    return true;
+if (numbers == undefined || numbers == []) {
+    return undefined;
+  } else if (numbers.length < 3 || numbers.some(isNaN)) {
+    return undefined;
   } else {
+
+    for (let i = 1; i < numbers.length; i++) {
+      if ((numbers[i - 1] + 1) === numbers[i]) {
+        i++
+        if ((numbers[i - 1] + 1) === numbers[i]) {
+          return true;
+          break;
+        } else {
+          continue;
+        }
+      } else {
+        continue;
+      }
+    }
     return false;
   }
-  // write your code here
 }
 
 function everywhere(values, x) {
@@ -177,20 +177,20 @@ function everywhere(values, x) {
     return false;
   }
 
-  // write your code here
+
 }
 
 function consecutive(numbers) {
   if (!numbers || numbers.length < 3) {
     return false;
   }
-  let numeral = true;
+  let numberthingy = true;
   for (i = 0; i <= numbers.length - 1; i++) {
     if (isNaN(numbers[i])) {
-      numeral = false;
+      numberthingy = false;
     }
   }
-  if (numeral === false) {
+  if (numberthingy === false) {
     return false;
   }
   let status = false;
@@ -209,20 +209,20 @@ function consecutive(numbers) {
   if (status === false) {
     return false;
   }
-  // write your code here
+   
 }
 
 function balance(numbers) {
   if (!numbers || numbers.length < 2) {
     return false;
   }
-  let numeral = true;
+  let numberthingy = true;
   for (i = 0; i <= numbers.length - 1; i++) {
     if (isNaN(numbers[i])) {
-      numeral = false;
+      numberthingy = false;
     }
   }
-  if (numeral === false) {
+  if (numberthingy === false) {
     return false;
   }
   let left = 0;
@@ -245,7 +245,7 @@ function balance(numbers) {
   if (count === 0) {
     return false;
   }
-  // write your code here
+   
   console.log("test");
 }
 
@@ -264,7 +264,7 @@ function clumps(values) {
   }
   return count;
   }
-  // write your code here
+   
 }
 
 /*
